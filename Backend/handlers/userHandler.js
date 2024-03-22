63fd13e9521bd7cbe37680f458a94622539b5a01
shivangi-jain-08 <shivangi.jain@kalvium.community>
2024-03-23 02:16:35 +0530

    completed a basic functioning backend for deployment

--- Backend/handlers/userHandler.js
+++ Backend/handlers/userHandler.js
@@ -1,4 +1,4 @@
-const User = require("../models/userModel");
+
 
 // Importing necessary modules
 const User = require("../models/userModel");
--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const connectToDB = require('./config/dbconnection');
+const blogRoute = require('./routes/blogRoute');
+const userRoute = require('./routes/userRoute');
 const app = express();
 const port = process.env.PORT || 3000;
 require('dotenv').config();
+
+
+app.use(cors({
+    origin: 'http://localhost:5174',
+    methods: 'GET, POST, PATCH, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  }));
 
 app.use(express.json());
 
@@ -20,6 +30,10 @@ connectToDB()
     .catch((err) => {
         console.error('Failed to connect to MongoDB', err);
     });
+
+
+    app.use("/api/data", blogRoute);
+    app.use("/api/users", userRoute);
 
 app.listen(port, () => {
     console.log(`🚀 Server running on PORT: ${port}`);
